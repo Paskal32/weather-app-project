2fc08edaa5bb9a97afe778a81590e81e053af247
Paskal32 <1paskal32@gmail.com>
2022-06-21 18:16:01 +0200

    build js. current temperature

---
+++ src/app.js
@@ -0,0 +1,29 @@
+function showTemperature(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+  document.querySelector("#max").innerHTML = Math.round(
+    response.data.main.temp_max
+  );
+  document.querySelector("#min").innerHTML = Math.round(
+    response.data.main.temp_min
+  );
+  document.querySelector("#time").innerHTML = formatDate(
+    response.data.dt * 1000
+  );
+}
+
+function search(city) {
+  let apiKey = "be5d707605e5661874f71c0e2f187224";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+search("London");
